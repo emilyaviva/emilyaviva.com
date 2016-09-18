e66945bfce743d8434e24e2f1d721ecf7249f440
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-09-17 19:46:34 -0700

    competencies flexbox on mobile

--- components/Competencies.js
+++ components/Competencies.js
@@ -11,18 +11,16 @@ const competencies = [
 const Competencies = () =>
   <main className='Competencies'>
     <h3>Competencies</h3>
-    <table>
-      <tbody>
-        {competencies.map((c, i) =>
-          <tr key={i}>
-            <td><strong>{c.name}</strong></td>
-            <td>
-              {c.values.map((v, j) => <span key={j} className='competency-value'>{v}</span>)}
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+      {competencies.map((c, i) =>
+        <div className='competencies-table' key={i}>
+          <div className='competency-name'>
+            <strong>{c.name}</strong>
+          </div>
+          <div className='competency-values'>
+            {c.values.map((v, j) => <span key={j} className='competency-value'>{v}</span>)}
+          </div>
+        </div>
+      )}
   </main>
 
 export default Competencies
