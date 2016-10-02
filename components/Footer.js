b61494323cf339f276b07b5fcf0b04581aeb6607
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2016-10-02 16:17:31 -0700

    add trailing slash to Aerobatic link

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const footerItems = [
   { href: 'github.com/gatsbyjs', icon: 'pencil', text: 'Built with Gatsby' },
   { href: 'github.com/emilyaviva/emilyaviva.com', icon: 'code-fork', text: 'This website is open-source' },
-  { href: 'www.aerobatic.com', icon: 'cogs', text: 'Static hosted by Aerobatic' }
+  { href: 'www.aerobatic.com/', icon: 'cogs', text: 'Static hosted by Aerobatic' }
 ]
 
 const Footer = () =>
